4ad0cf77585c64ee6bfef81a491498cbc36bda9d
jspkay <jspkay@gmail.com>
2022-06-23 17:11:35 +0200

    Added graph generator function

---
+++ projectII/generate_adj_mtx.m
@@ -0,0 +1,20 @@
+function mtx = generate_adj_mtx(type, n)
+
+if type == 'chain'
+    switch(c)
+    case 1
+        % Chain 1
+    case 2
+        % Chain 2
+    case 3
+        % Chain
+    end
+elseif type == 'tree'
+    
+elseif type == 'complete'
+    
+elseif type == 'dicatator'
+    
+end
+
+end
